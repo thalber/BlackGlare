1f2b7a88ac73157ca95b971cb3d324294b6d9cb4
thalber <filefillman@gmail.com>
2024-01-10 13:06:34 +0400

    document api

--- src/API/LabelDefinition.cs
+++ src/API/LabelDefinition.cs
@@ -1,5 +1,10 @@
 namespace BlackGlare.API;
 
+/// <summary>
+/// Rules and text callbacks for a label. Wraps internal types.
+/// </summary>
+/// <typeparam name="TItem">Type of all items being filtered and labelled.</typeparam>
+/// <typeparam name="TDown">Type the items have to belong to to actually get labels.</typeparam>
 public class LabelDefinition<TItem, TDown>
 	where TItem : notnull
 	where TDown : notnull
@@ -14,7 +19,11 @@ public class LabelDefinition<TItem, TDown>
 			selector,
 			Guid.NewGuid(),
 			(obj) => describe((TDown)(object)obj));
+
 	}
+	/// <summary>
+	/// Adds an extra condition to make the label appear.
+	/// </summary>
 	public LabelDefinition<TItem, TDown> AddCondition(Func<TDown, bool> condition)
 	{
 		innerSelector &= new Selector<TDown>.ByCallback(condition);
--- src/API/Labels.cs
+++ src/API/Labels.cs
@@ -1,7 +1,16 @@
 namespace BlackGlare.API;
 
+/// <summary>
+/// Use static methods here to add labels to things.
+/// </summary>
 public static class Labels
 {
+	/// <summary>
+	/// Adds a label to specified type of physicalobject.
+	/// </summary>
+	/// <param name="describe">Callback that returns the label text.</param>
+	/// <typeparam name="TObj">Type of physicalobject that should have the label. Can be an interface.</typeparam>
+	/// <returns>LabelDefinition object that can be used to add extra conditions.</returns>
 	public static LabelDefinition<PhysicalObject, TObj> AddObjectLabel<TObj>(Func<TObj, string?> describe)
 		where TObj : notnull
 	{
@@ -11,6 +20,11 @@ public static class Labels
 		mod.realEntityMessages.AddDescriptor(result.actual);
 		return result;
 	}
+	/// <summary>
+	/// Adds a label to room panels in devtools devmap view.
+	/// </summary>
+	/// <param name="describe">Callback that returns the label text.</param>
+	/// <returns>LabelDefinition object that can be used to add extra conditions.</returns>
 	public static LabelDefinition<AbstractRoom, AbstractRoom> AddRoomLabel(Func<AbstractRoom, string?> describe)
 	{
 		PluginNotInitializedException.ThrowIfNotInitialized();
